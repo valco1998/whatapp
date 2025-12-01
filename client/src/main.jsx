51138a197c32cfcd2c1c496d12b6c96d80aeb10d
Idan Akiva <akivaidan1998@gmail.com>
2025-12-01 12:07:25 +0200

    Fix imports + update signup hook

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+
 import { AuthContextProvider } from "./context/authContext.jsx";
 import { SocketContextProvider } from "./context/SocketContext.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
